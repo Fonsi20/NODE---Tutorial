ea6ca880ccc684609b941e6e75213c3f3122d5a3
Fonsi <alfonso@crescent-integrations.com>
2019-04-08 11:23:49 +0200

    Creador Modulo propio de hora y fecha

--- helloWorldNode.js
+++ helloWorldNode.js
@@ -1,6 +1,16 @@
 var http = require('http');
+//Llamo a mi modulo propio
+var dt = require('./myfirstModule');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+
+    //Para seleccionar donde quiero que aparezca la informacion
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    //Para generar el texto con el hora y el dia tras llamar a a mi propio modulo de hora y fecha
+    res.write("The date and time are currently: " + dt.myDateTime());
+
+    //Sacar un mensaje cualquiera
+    res.end('Hello World!');
+
 }).listen(8080);
